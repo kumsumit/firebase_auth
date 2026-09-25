3a652fcd60b9534e9e76d3a2fdfcf26d1c59db20
sumitsharansatsangi <sharansumitkumar@gmail.com>
2026-09-25 22:34:23 +0530

    code updated

--- android/local-config.gradle.kts
+++ android/local-config.gradle.kts
@@ -2,5 +2,5 @@ extra.apply {
     set("compileSdk", 37)
     set("minSdk", 23)
     set("javaVersion", JavaVersion.VERSION_21)
-    set("androidGradlePluginVersion", "9.4.0")
+    set("androidGradlePluginVersion", "9.4.1")
 }
